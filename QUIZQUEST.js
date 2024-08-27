39cc63dbcb3b75bd6c624c5c55c40670b4f66094
Sulton <mcphersonleo13@gmail.com>
2024-08-27 16:55:44 +0100

    Programmers Quiz

---
+++ QUIZQUEST.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "WHO INVENTED PYTHON?",
+        answer: "C.  Guido van Rossum ",
+        options: [
+            "A. ADA",
+            "B. SULTON",
+            "C.  Guido van Rossum ",
+            "D. Charles Barbage"
+        ]
+    },
+    {
+        numb: 2,
+        question: "WHO IS THE FATHER OF COMPUTER?",
+        answer: "D. CHARLES BARBAGE",
+        options: [
+            "A. CHARLES DARWIN",
+            "B. THOMAS BARBAGE",
+            "C. SALZ SAVAGE",
+            "D. CHARLES BARBAGE"
+        ]
+    },
+    {
+        numb: 3,
+        question: "WHO INVENTED C?",
+        answer: "C. Dennis Ritchie",
+        options: [
+            "A. Ada Lovelace",
+            "B. Karl Mars",
+            "C. Dennis Ritchie",
+            "D. Elon Musk"
+        ]
+    },
+    {
+        numb: 4,
+        question: "WHICH LANGUAGE CAN BE USED FOR BOTH FRONTEND AND BACKEND?",
+        answer: "C. JAVASCRIPT",
+        options: [
+            "A. C", 
+            "B. JAVA",
+            "C. JAVASCRIPT",
+            "D. RUBY"
+        ]
+    },
+    {
+        numb: 5,
+        question: "WHICH IS NOT A PROGRAMMING LANGUAGE",
+        answer: "B. C+",
+        options: [
+            "A. C",
+            "B. C+",
+            "C. C++",
+            "D. C#"
+        ]
+    },
+    {
+        numb: 6,
+        question: "WHICH LANGUAGE IS USED FOR DATABASE MANAGEMENT?",
+        answer: "B. SQL",
+        options: [
+            "A. JAVASCRIPT",
+            "B. SQL",
+            "C. RUBY",
+            "D. PHP"
+        ]
+    },
+    {
+        numb: 7,
+        question: "WHICH IS THE FASTEST PROGRAMMING LANGUAGE?",
+        answer: "A. C++",
+        options: [
+            "A. C++",
+            "B. JAVA",
+            "C. JAVASCRIPT",
+            "D. PASCAL"
+        ]
+    },
+    {
+        numb: 8,
+        question: "WHATS THE FIRST COMPUTER PROGRAMMING LANGUAGE?",
+        answer: "D. ADA",
+        options: [
+            "A. JAVA",
+            "B. C",
+            "C. FOTRAIN",
+            "D. ADA"
+        ]
+    },
+    {
+        numb: 9,
+        question: "WHO INVENTED PROGRAMMING LANGUAGE?",
+        answer: "D. ADA LOVELACE",
+        options: [
+            "A. CHARLES BARBAGE", 
+            "B. BENEDICT CUCUMBATCH",
+            "C. DENNIE RITCHIE",
+            "D. ADA LOVELACE"
+        ]
+    },
+    {
+        numb: 10,
+        question: "WHICH PROGRAMMING LANGUAGE IS USED FOR GAME DEVELOPMENT?",
+        answer: "D. ALL OF THE ABOVE",
+        options: [
+            "A. C++",
+            "B. JAVA",
+            "C. PYTHON",
+            "D. ALL OF THE ABOVE"
+        ]
+    },
+];
